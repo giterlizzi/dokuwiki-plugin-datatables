62f5a449d2daeba1e2f95a0786b167384fff8c3f
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2016-02-11 10:38:08 +0100

    Added fallback and warning message in "preview" for malformed "<datatable/>" tag

--- syntax.php
+++ syntax.php
@@ -13,67 +13,76 @@ if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
 class syntax_plugin_datatables extends DokuWiki_Syntax_Plugin {
 
-    function getType(){ return 'container';}
-    // Added susbstition allowed type, so that Doku renders tables from other plugins, then DataTable can be applied on them.
-    function getAllowedTypes() { return array('container', 'substition'); }
-    function getPType(){ return 'block';}
-    function getSort(){ return 195; }
+  function getType(){ return 'container';}
+  function getAllowedTypes() { return array('container', 'substition'); }
+  function getPType(){ return 'block';}
+  function getSort(){ return 195; }
 
-    function connectTo($mode) {
-      $this->Lexer->addEntryPattern('<(?:DATATABLES?|datatables?)\b.*?>(?=.*?</(?:DATATABLES?|datatables?)>)', $mode, 'plugin_datatables');
+  function connectTo($mode) {
+    $this->Lexer->addEntryPattern('<(?:DATATABLES?|datatables?)\b.*?>(?=.*?</(?:DATATABLES?|datatables?)>)', $mode, 'plugin_datatables');
+  }
+
+  public function postConnect() {
+    $this->Lexer->addExitPattern('</(?:DATATABLES?|datatables?)>', 'plugin_datatables');
+  }
+
+  function handle($match, $state, $pos, Doku_Handler $handler) {
+
+    switch ($state) {
+      case DOKU_LEXER_ENTER     : return array($state, $match);
+      case DOKU_LEXER_UNMATCHED : return array($state, $match);
+      case DOKU_LEXER_EXIT      : return array($state, $match);
     }
 
-    public function postConnect() {
-      $this->Lexer->addExitPattern('</(?:DATATABLES?|datatables?)>', 'plugin_datatables');
-    }
+    return array();
 
-    function handle($match, $state, $pos, Doku_Handler $handler) {
+  }
 
-      switch ($state) {
-        case DOKU_LEXER_ENTER     : return array($state, $match);
-        case DOKU_LEXER_UNMATCHED : return array($state, $match);
-        case DOKU_LEXER_EXIT      : return array($state, $match);
-      }
+  function render($mode, Doku_Renderer $renderer, $data) {
 
-      return array();
+    if (empty($data))      return false;
+    if ($mode !== 'xhtml') return false;
 
-    }
+    /** @var Doku_Renderer_xhtml $renderer */
 
-    function render($mode, Doku_Renderer $renderer, $data) {
+    list($state, $match) = $data;
 
-      if (empty($data)) return false;
+    switch($state) {
 
-      if ($mode == 'xhtml') {
+      case DOKU_LEXER_ENTER:
 
-        /** @var Doku_Renderer_xhtml $renderer */
+        $html5_data = array();
+        $xml = @simplexml_load_string(str_replace('>', '/>', $match));
 
-        list($state, $match) = $data;
+        if (! is_object($xml)) {
 
-        switch($state) {
-          case DOKU_LEXER_ENTER:
-
-            $html5_data = array();
-            $xml = simplexml_load_string(str_replace('>', '/>', $match));
-
-            foreach ($xml->attributes() as $key => $value) {
-              $html5_data[] = sprintf("data-%s='%s'", $key, str_replace("'", "&apos;", (string) $value));
-            }
-
-            $renderer->doc .= sprintf('<div class="dt-wrapper" %s>', implode(' ', $html5_data));
-            return true;
-
-          case DOKU_LEXER_EXIT:
-            $renderer->doc .= '</div>';
-            return true;
-
-          case DOKU_LEXER_UNMATCHED:
-            $renderer->doc .= $match;
-            return true;
+          $xml = simplexml_load_string('<foo />');
+  
+          global $ACT;
+  
+          if ($ACT == 'preview') {
+            msg(sprintf('<strong>DataTable Plugin</strong> - Malformed tag (%s). Please check your code!', hsc($match)), -1);
+          }
 
         }
 
-      }
+        foreach ($xml->attributes() as $key => $value) {
+          $html5_data[] = sprintf("data-%s='%s'", $key, str_replace("'", "&apos;", (string) $value));
+        }
+
+        $renderer->doc .= sprintf('<div class="dt-wrapper" %s>', implode(' ', $html5_data));
+        return true;
+
+      case DOKU_LEXER_EXIT:
+        $renderer->doc .= '</div>';
+        return true;
+
+      case DOKU_LEXER_UNMATCHED:
+        $renderer->doc .= $match;
+        return true;
 
     }
+
+  }
 
 }
